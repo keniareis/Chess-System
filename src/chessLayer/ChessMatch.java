866f52d3aacb80f836c5afd528973019e536c524
Kenia de Oliveira <keniapubg@gmail.com>
2024-01-20 15:50:43 -0300

    manipulando as peças capturadas

--- src/chessLayer/ChessMatch.java
+++ src/chessLayer/ChessMatch.java
@@ -6,6 +6,9 @@ import boardLayer.Position;
 import chessLayer.pieces.King;
 import chessLayer.pieces.Rook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMatch {
     private Board board;
     private int turn;
@@ -15,6 +18,8 @@ public class ChessMatch {
     private ChessPiece enPassantVulnerable;
     private ChessPiece promoted;
 
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
 
     public ChessMatch(){
         board = new Board(8, 8);
@@ -81,6 +86,12 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+
+        if (capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
+
         return capturedPiece;
     }
 
@@ -91,6 +102,7 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     private void initialSetap(){
