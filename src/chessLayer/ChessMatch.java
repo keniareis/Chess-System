bfe24619bbf084449ff419ec7e67799197a78f4b
Kenia de Oliveira <keniapubg@gmail.com>
2024-01-23 12:31:28 -0300

    adicionando o bispo

--- src/chessLayer/ChessMatch.java
+++ src/chessLayer/ChessMatch.java
@@ -3,6 +3,7 @@ package chessLayer;
 import boardLayer.Board;
 import boardLayer.Piece;
 import boardLayer.Position;
+import chessLayer.pieces.Bishop;
 import chessLayer.pieces.King;
 import chessLayer.pieces.Pawn;
 import chessLayer.pieces.Rook;
@@ -193,7 +194,9 @@ public class ChessMatch {
 
     private void initialSetap(){
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -205,7 +208,9 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
